8bf80bbf9abf7c083c8932209bbdcd3cd21f08fe
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-03-04 16:37:27 +0100

    grid of images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import testImage from './images/testKarte.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='flex-container'>
+        <div className='flex-container'>
+          <div className='flex-item'>
+            <img src={testImage}></img>
+          </div>
+          <div className='flex-item'>
+            <img src={testImage}></img>
+          </div>
+        </div>
+        <div className='flex-container'>
+          <div className='flex-item'>
+            <img src={testImage}></img>
+          </div>
+          <div className='flex-item'>
+            <img src={testImage}></img>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
