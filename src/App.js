c253e1f258519745c5d9a7bb3bf169357ba28603
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-03-08 09:37:08 +0100

    implemented memory board, simpler structure

--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import './App.css';
 import testImage from './images/testKarte.png';
 
 function App() {
+
+  const images = [
+    testImage, testImage, testImage, testImage
+  ];
+
+
   return (
-    <div className="App">
-      <div className='flex-container'>
-        <div className='flex-container'>
-          <div className='flex-item'>
-            <img src={testImage}></img>
-          </div>
-          <div className='flex-item'>
-            <img src={testImage}></img>
-          </div>
-        </div>
-        <div className='flex-container'>
-          <div className='flex-item'>
-            <img src={testImage}></img>
-          </div>
-          <div className='flex-item'>
-            <img src={testImage}></img>
-          </div>
+    <div className='App'>
+      <div className='memory-board'>
+        <div className="image-grid">
+          {images.map((image, index) => (
+              <img key={index} src={image} className='image' />
+            ))
+          }
         </div>
       </div>
     </div>
